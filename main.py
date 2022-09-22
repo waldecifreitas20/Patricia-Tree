e450dd0dd2bfa270349d481c1cddd1631194692e
israel silva <Israel524.is@gmail.com>
2022-09-22 15:46:55 -0300

    menu preview
    OBS: opções 3 e 4 não estão implementadas.

--- main.py
+++ main.py
@@ -11,15 +11,44 @@ pt = PatriciaTree()
 
 search = 'sal'
 
-pt.insert('romane')
-pt.insert('rubicon')
-pt.insert('romanus')
 
-pt.insert('rubens')
-pt.insert('ruber')
+#   -----INICIO MENU------
+opcao = None
+while True:
+    print("inicio laço!!!")
+    if opcao == None:
+        pt.menu()
+        opcao = int(input("Opção:"))
 
-pt.print()
-print("teste ")
+    elif opcao == 1:
+        pt.clear_display()
+
+        print("Arvore atual: \n")
+        pt.print()
+        print("\n")
+        palavra = str(input("Digite sua palavra:"))
+
+        pt.clear_display()
+        pt.insert(palavra)
+        print("Arvore atual: \n")
+        pt.print()
+        print("\n")
+        print("2 - Ver Arvore")
+        print("1 - inserir")
+        print("0 - Encerar")
+        opcao = int(input("\nOpção:"))
+
+    elif opcao == 2:
+        pt.clear_display()
+        print("Arvore atual: \n")
+        pt.print()
+        print("\n")
+        print("1 - voltar")
+        opcao = int(input("\nOpção:"))
+        opcao = None
+    if opcao == 0:
+        break
+#   -----FIM MENU------
 
 
 
